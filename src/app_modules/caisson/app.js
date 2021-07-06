c1b74a71b60b07e592ab386c0114196aa349fcc6
Pip Grylls <pipgryllsastro@gmail.com>
2021-07-06 12:33:18 +0100

    input updates closing #1

--- src/app_modules/caisson/app.js
+++ src/app_modules/caisson/app.js
@@ -1,80 +1,108 @@
-import { LitElement, html } from 'lit';
-import { we4eStyles, we4eGrids} from '../../styles/we4eStyles.js';
-import {caissonConf as appConf} from '../moduleConf.js';
-import {calculateCaisson as appCalc} from '../../../../wa4e-v2-maths/output/wa4e-math.js';
+import { html, LitElement } from 'lit';
+import { we4eGrids, we4eStyles } from '../../styles/we4eStyles.js';
+import { caissonConf as appConf } from '../moduleConf.js';
+import { calculateCaisson as appCalc } from '../../../../wa4e-v2-maths/output/wa4e-math.js';
 import '../../elements/myElements.js';
 
 export class App extends LitElement {
   static get properties() {
     return {
-      appWebComponents: { type: Object},
+      appWebComponents: {
+        type: Object,
+      },
       title: { type: String },
-      devMessage: {type: String},
-      output: {type: Object}
+      output: { type: Object },
+      appTiles: {
+        type: html,
+      },
     };
   }
 
   static get styles() {
-    return [
-      we4eStyles,
-      we4eGrids
-    ]
+    return [we4eStyles, we4eGrids];
   }
 
   constructor() {
     super();
     this.title = appConf.appPageTitle;
     this.grid = appConf.appGrid;
-    this.appHTML = html``;
-    this.devMessage = "";
+    this.devMessage = '';
     this.appWebComponents = appConf.appWebComponents;
     this.output = {};
+    this.appTiles = this.makeAppTiles();
   }
 
   render() {
-    this.appTiles =
-      html`${this.appWebComponents.map((component) =>
-        html`
-        ${component.type==="input-tile"?
-            html`<input-tile .appConf="${component}" @updated='${(e) => { this.devMessage = e.detail.message; this.updateComponents() }}'></input-tile>`:
-          component.type==="derived-input-tile"?
-            html`<derived-input-tile .appConf="${component}"></derived-input-tile>`:
-          component.type==="output-tile"?
-            html`<output-tile .appConf="${component}"></output-tile>`:
-          component.type==="image-tile"?
-            html`<image-tile .appConf="${component}"></image-tile>`:
-          component.type==="graph-tile"?
-            html`<graph-tile .appConf="${component}"></graph-tile>`:
-          component.type==="coeff-tile"?
-            html`<graph-tile .appConf="${component}"></graph-tile>`:
-          component.type==="optimisation-tile"?
-            html`<graph-tile .appConf="${component}"></graph-tile>`:
-          html`<p>Component ${component.type} Not Recognised</p>`
-        }`
-      )}`;
-
-    this.appHTML = html`
+    console.log('main render');
+    console.log(this.appWebComponents);
+    console.log(this.appTiles);
+    return html`
       <header-element page-title=${this.title}></header-element>
-      <div class='grid_container' style='--x:${this.grid.x};--y:${this.grid.y};'>
       ${this.appTiles}
-      </div>
-      <p>${this.devMessage}</p>
       <footer-element></footer-element>
     `;
-
-    return this.appHTML;
   }
 
   updateComponents() {
-    this.output = appCalc(this.appWebComponents.find(element => element.type === 'input-tile'), 0, 0);
+    this.output = appCalc(
+      this.appWebComponents.find(element => element.type === 'input-tile'),
+      0,
+      0
+    );
     for (const [key, value] of Object.entries(this.output.derivedInputs)) {
-      this.appWebComponents.find(element => element.type === 'derived-input-tile').fields[key][0] = value;
+      this.appWebComponents.find(
+        element => element.type === 'derived-input-tile'
+      ).fields[key][0] = value;
     }
-    this.devMessage = "Output Generated";
+    console.log('maths is done');
   }
 
+  /* eslint-disable no-nested-ternary */
+  makeAppTiles() {
+    return html`${this.appWebComponents.map(
+      (component, index) =>
+        html` <div
+          id="tiles"
+          class="grid_container"
+          style="--x:${this.grid.x};--y:${this.grid.y};"
+        >
+          ${component.type === 'input-tile'
+            ? html`<input-tile
+                .appConf=${this.appWebComponents[index]}
+                @updated="${() => {
+                  this.updateComponents();
+                }}"
+              ></input-tile>`
+            : component.type === 'derived-input-tile'
+            ? html`<derived-input-tile
+                id="derived-input-tile"
+                .appConf=${this.appWebComponents[index]}
+              ></derived-input-tile>`
+            : component.type === 'output-tile'
+            ? html`<output-tile
+                .appConf=${this.appWebComponents[index]}
+              ></output-tile>`
+            : component.type === 'image-tile'
+            ? html`<image-tile
+                .appConf=${this.appWebComponents[index]}
+              ></image-tile>`
+            : component.type === 'graph-tile'
+            ? html`<graph-tile
+                .appConf=${this.appWebComponents[index]}
+              ></graph-tile>`
+            : component.type === 'coeff-tile'
+            ? html`<graph-tile
+                .appConf=${this.appWebComponents[index]}
+              ></graph-tile>`
+            : component.type === 'optimisation-tile'
+            ? html`<graph-tile
+                .appConf=${this.appWebComponents[index]}
+              ></graph-tile>`
+            : html`<p>Component ${component.type} Not Recognised</p>`}
+        </div>`
+    )}`;
+  }
+  /* eslint-enable no-nested-ternary */
 }
 
-customElements.define(`${appConf.appName}-app`, App)
-
-
+customElements.define(`${appConf.appName}-app`, App);
