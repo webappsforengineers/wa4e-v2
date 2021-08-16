de2bc1bfd8374317ad45df065df66d12f46d27a7
Pip Grylls <pipgryllsastro@gmail.com>
2021-08-16 14:47:59 +0100

    writing mcc-su config

--- public/app_modules/mcc-su/appConfig.js
+++ public/app_modules/mcc-su/appConfig.js
@@ -1,46 +1,153 @@
 export const appConf = {
-  "appName": "mcc-su",
-  "appTitle": "MCC — su",
-  "appPageTitle": "",
-  "appDescription": "Conversion of MCC parameters to su profile",
-  "appWebComponents": {
-    "aComponent": {
-      "type": "input",
-      "size": {
-        "x": 1,
-        "y": 4
-      },
-      "position": {
-        "x": 0,
-        "y": 0
-      },
-      "fields": {
-        "aSubsection": {
-          "Mass": "kg",
-          "Volume": "m3",
-          "ratio": ""
+  appName: 'mcc-su',
+  appTitle: 'MCC — su',
+  appPageTitle: '',
+  appDescription: 'Conversion of MCC parameters to su profile',
+  appWebComponents: [
+    {
+      type: 'input-tile',
+      title: 'Input',
+      fields: {
+        '': {
+          lambda: [0.205, null, '&lambda;'],
+          kappa: [0.044, null, '&kappa;'],
+          M: [0.89, null, 'M'],
+          e_cs: [2.14, null, 'e<sub>cs</sub>'],
+          v_prime: [0.3, null, 'v&#39;'],
+          G_s: [2.7, null, 'G<sub>s</sub>'],
+          g: [9.81, 'm/s<sup>2</sup>', 'g'],
+          gamma_w: [9.81, 'kN/m<sup>3</sup>', '&gamma;<sub>w</sub>'],
+          sigma_prime_v0: [1, 'kPa', '&sigma;&#39;<sub>v0</sub>'],
+          depth_of_interest: [10, 'm', 'DoI'],
         },
-        "bSubsection": {
-          "thing": "unit"
-        }
       },
-      "buttons": {
-        "SUBMIT": "green",
-        "RESET": "",
-        "HELP": ""
-      }
+      helpText: 'Helpful text!',
     },
-    "bComponent": {
-      "type": "Image",
-      "size": {
-        "x": 2,
-        "y": 2
+    {
+      type: 'derived-input-tile',
+      title: 'Derived Input',
+      fields: {
+        '': {
+          e_1: [null, null, 'e<sub>1</sub>'],
+          phi_prime_cs_ps: [null, 'radians', '&phi;&#39;<sub>cs,ps</sub>'],
+          phi_prime_cs_ps_degrees: [null, 'degrees', null],
+          phi_prime_cs_txc: [null, 'radians', '&phi;&#39;<sub>cs,txc</sub>'],
+          phi_prime_cs_txc_degrees: [null, 'degrees', null],
+          K_0: [null, null, 'K<sub>0</sub>'],
+          gamma_sat_av: [null, 'kN/m<sup>3</sup>', '&gamma;<sub>sat,av</sub>'],
+          gamma_prime_av: [
+            null,
+            'kN/m<sup>3</sup>',
+            '&gamma;&#39;<sub>av</sub>',
+          ],
+        },
       },
-      "position": {
-        "x": 0,
-        "y": 4
+    },
+    {
+      type: 'output-tile',
+      title: 'Output',
+      fields: {
+        'Plane Strain (PS)': {
+          s_um_ps: [null, 'kPa', 's<sub>um</sub>'],
+          k_ps: [null, 'kPa/m', 'k'],
+        },
+        'Triaxial (TX)': {
+          s_um_tx: [null, 'kPa', 's<sub>um</sub>'],
+          k_tx: [null, 'kPa/m', 'k'],
+        },
       },
-      "imgPath": "./mcc-su.png"
-    }
-  }
-}
+    },
+    {
+      type: 'image-tile',
+      img_pth: 'mcc-su-figure.png',
+      img_w: '496px',
+      img_h: '210px',
+    },
+    {
+      type: 'graph-tile',
+      fields: {
+        z: null,
+        sigma_prime_v: null,
+        u_hyd: null,
+        sigma_v: null,
+        sigma_prime_h: null,
+        PS_s_u: null,
+        TX_s_u: null,
+      },
+      plots: {
+        inSituStresses: {
+          dataFun(a, b, c, d, e) {
+            return [
+              {
+                x: a,
+                y: b,
+              },
+              {
+                x: a,
+                y: c,
+              },
+              {
+                x: a,
+                y: d,
+              },
+              {
+                x: a,
+                y: e,
+              },
+            ];
+          },
+          layout: {
+            title: '',
+            xaxis: {
+              title: 'S<sub>u</sub> (kPa)',
+              side: 'top',
+              rangemode: 'tozero',
+            },
+            yaxis: {
+              title: 'z (m)',
+              autorange: 'reversed',
+            },
+            showlegend: false,
+            mode: 'lines',
+            line: { shape: 'spline' },
+          },
+          args: ['z', 'sigma_prime_v', 'u_hyd', 'sigma_v', 'sigma_prime_h'],
+          addLines: false,
+          data: [],
+        },
+        inSituUndrainedStresses: {
+          dataFun(a, b, c) {
+            return [
+              {
+                x: a,
+                y: b,
+              },
+              {
+                x: a,
+                y: c,
+              },
+            ];
+          },
+          layout: {
+            title: '',
+            xaxis: {
+              title: 'S<sub>u</sub> (kPa)',
+              side: 'top',
+              rangemode: 'tozero',
+            },
+            yaxis: {
+              title: 'z (m)',
+              autorange: 'reversed',
+            },
+            showlegend: false,
+            mode: 'lines',
+            line: { shape: 'spline' },
+          },
+          args: ['z', 'PS_s_u', 'TX_s_u'],
+          addLines: false,
+          data: [],
+        },
+      },
+    },
+  ],
+};
