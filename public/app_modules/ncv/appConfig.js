ea80f8e3f6175174060534e4a9bde59e5b28c870
Pip Grylls <pipgryllsastro@gmail.com>
2021-08-17 14:57:46 +0100

    adding ncv

--- public/app_modules/ncv/appConfig.js
+++ public/app_modules/ncv/appConfig.js
@@ -1,21 +1,37 @@
 export const appConf = {
   appName: 'ncv',
   appTitle: 'Shallow Foundation NcV',
-  appPageTitle: '',
+  appPageTitle: 'N<sub>cV</sub> for Shallow Foundation',
   appDescription:
     'Undrained vertical bearing capacity of strip and circular skirted foundations',
   appWebComponents: [
     {
+      type: 'input-tile',
+      title: 'Input',
+      fields: {
+        'Foundation Properties': {
+          B_D: [],
+          d: [],
+        },
+        'Soil Properties': {
+          sum: [],
+          k: [],
+        },
+      },
+    },
+    {
       type: 'derived-input-tile',
       title: 'Derived Input',
       fields: {
-        dD: [null, null, 'd/D (0&#45;1)'],
-        A: [null, 'm<sup>2</sup>sup>', 'A'],
-        kappsu: [
-          null,
-          null,
-          '&varkappa;<sub>su</sub> = kD/s<sub>um</sub> (0&#45;200)',
-        ],
+        '': {
+          dD: [null, null, 'd/D (0&#45;1)'],
+          A: [null, 'm<sup>2</sup>sup>', 'A'],
+          kappsu: [
+            null,
+            null,
+            '&varkappa;<sub>su</sub> = kD/s<sub>um</sub> (0&#45;200)',
+          ],
+        },
       },
     },
   ],
