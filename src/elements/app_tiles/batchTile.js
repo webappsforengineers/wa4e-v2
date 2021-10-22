861043a3be6ba77416a9d7565fc8601dbefc9e99
Pip Grylls <pipgryllsastro@gmail.com>
2021-10-22 14:35:13 +0100

    filedrop working needs styling

--- src/elements/app_tiles/batchTile.js
+++ src/elements/app_tiles/batchTile.js
@@ -1,21 +1,24 @@
 import { html } from 'lit';
 import { saveAs } from 'file-saver-es';
 import { toString } from 'lodash-es';
-import { Dropzone } from '../../local_modules/wa4e-math.js'
+//import * as URL from 'url';
+
+// eslint-disable-next-line import/extensions
 import { TileBase } from './tileBase';
 
 
 
 class batchTile extends TileBase {
+
   render() {
     // We need this as batch-tile has the unique property of needing all the app components
     this.localAppConf = this.appConf.find(element => element.type === 'batch-tile');
     this.formFields = this.appConf.find(element => element.type === 'input-tile').fields;
-    Dropzone.options.myDropzone = { maxFiles:1, addRemoveLinks:true };
+    this.fileData = null;
     return [
       super.render(),
       html`
-        <link rel="stylesheet" href="../../../node_modules/dropzone/dist/dropzone.css" />
+
         <h2>${html([this.localAppConf.title])}</h2>
         <h4>1. Get the template</h4>
         <button
@@ -24,11 +27,84 @@ class batchTile extends TileBase {
         >Download Template</button>
         <h4>2. Fill the template.</h4>
         <p>Each column is a single calculation that will be run. Columns must be complete. Do not edit the generated fields. Any data that extends below the generated fields will not be used.</p>
-        <h4>Upload the file</h4>
-        <form id='myDropzone' action="/target" class="dropzone"></form>
+        <h4>3. Upload the file</h4>
+        <input type="file" id="dropbox" accept=".csv">
+        <button
+          id='dropbox-button'
+          class="btn btn-outline-secondary col"
+        >Submit file</button>
       `
     ];
   }
+
+  firstUpdated(_changedProperties) {
+    super.firstUpdated(_changedProperties);
+    let dropbox;
+    dropbox = document.getElementById("dropbox");
+    dropbox.addEventListener("change", (e) => this.clickFile(e), false);
+    dropbox.addEventListener("dragenter", this.dragenter, false);
+    dropbox.addEventListener("dragover", this.dragover, false);
+    dropbox.addEventListener("drop", this.drop, false);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  dragenter(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  dragover(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  drop(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    const dt = e.dataTransfer;
+    const files = dt.files;
+
+    if (files.length !== 1) {
+      window.alert("Single file upload only. All files removed.")
+    } else {
+      this.handleFile(files[0]);
+    }
+  }
+
+  clickFile(e){
+    const {files} = e.currentTarget
+    if (files.length !== 1) {
+      window.alert("Single file upload only. All files removed.")
+    } else {
+      this.handleFile(files[0]);
+    }
+  }
+
+  async handleFile(fileObj) {
+
+    if (this.fileData !== null) {
+      window.alert("Single file upload only. Previous file is removed.")
+    }
+
+    const filePromise = fileObj.text();
+    filePromise.then(
+      (value) => {
+        /* code if successful */
+        // write the value of the file to fileData
+        this.fileData = value;
+      },
+      (error) => {
+        /* code if some error */
+        window.alert(`file load failed with error ${error}`);
+        // remove all data from fileData
+        this.fileData = null;
+      }
+    );
+  }
+
 
   /**
    * Create an input csv with each input tile input field as a column and each row as an input to run
@@ -53,8 +129,10 @@ class batchTile extends TileBase {
   }
 
   runCalc() {
-
+    document.getElementById("dropbox").value='';
   }
+
+
 }
 
 customElements.define('batch-tile', batchTile);
