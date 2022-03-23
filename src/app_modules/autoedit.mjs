6e52bb0ef7074a2dfb59e11c4116588522a98a47
Pip Grylls <pipgryllsastro@gmail.com>
2022-03-23 15:25:25 +0000

    testing more powerful autoedit

--- src/app_modules/autoedit.mjs
+++ src/app_modules/autoedit.mjs
@@ -1,34 +1,66 @@
-import { appConf } from './zti/appConfig.mjs';
 import * as fs from 'fs';
 
+import {
+  caissonConf,
+  consolidatedncvConf,
+  dragAnchorConf,
+  mccsuConf,
+  ncvConf,
+  pinpilesConf,
+  pipeConf,
+  spletConf,
+  vh2m2tConf,
+  vhmConf,
+  ztiConf,
+} from './moduleConf.mjs';
 
-let modFields = ['input-tile', 'derived-input-tile', 'output-tile'];
-for (let component of appConf.appWebComponents) {
-  let new_fields = {};
-  let modFieldIdx = modFields.indexOf(component.type);
-  if (modFieldIdx >= 0) {
-    const groups = Object.entries(component.fields);
-    groups.forEach(([grp_key, value]) => {
-      let new_obj = {};
-      const entries = Object.entries(value);
-      entries.forEach(([entry_name, item_values]) => {
-        new_obj[entry_name] = {
-          label: item_values[2],
-          unit: item_values[1],
-          value: item_values[0],
-          visible: item_values[3],
-          lb: '',
-          ub: '',
-        }
+const apps = [caissonConf,
+  consolidatedncvConf,
+  dragAnchorConf,
+  mccsuConf,
+  ncvConf,
+  pinpilesConf,
+  pipeConf,
+  spletConf,
+  vh2m2tConf,
+  vhmConf,
+  ztiConf]
+
+for (let x in apps) {
+  let appConf = apps[x]
+  let modFields = ['optimization-tile'];
+  for (let component of appConf.appWebComponents) {
+    let new_fields = {};
+    let modFieldIdx = modFields.indexOf(component.type);
+    if (modFieldIdx >= 0) {
+      const groups = Object.entries(component.fields);
+      groups.forEach(([grp_key, value]) => {
+        let new_obj = {};
+        const entries = Object.entries(value);
+        entries.forEach(([entry_name, item_values]) => {
+          new_obj[entry_name] = {
+            label: item_values[2],
+            unit: item_values[1],
+            value: item_values[0],
+            visible: item_values[3],
+            lb: '',
+            ub: '',
+          }
+        });
+        new_fields[grp_key] = new_obj
       });
-      new_fields[grp_key] = new_obj
-    });
-    let mod_this = appConf.appWebComponents.find(element => element.type === modFields[modFieldIdx])
-    mod_this.fields = new_fields;
+      let mod_this = appConf.appWebComponents.find(element => element.type === modFields[modFieldIdx])
+      mod_this.fields = new_fields;
+    }
   }
+
+  let newText = "/* eslint-disable no-sparse-arrays */\n" +
+    "export const appConf = " + JSON.stringify(appConf, null, 2)
+
+  fs.writeFile("./"+appConf.appName+"/appConfigTest.mjs", newText, (err) => {
+    // In case of a error throw err.
+    if (err) throw err;
+  });
 }
 
-fs.writeFile("./zti/new_conf.txt", JSON.stringify(appConf, null, 3), (err) => {
-  // In case of a error throw err.
-  if (err) throw err;
-});
+
