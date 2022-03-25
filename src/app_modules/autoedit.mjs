7c8eb57fa8f168845b88375c28138c505431a9aa
Pip Grylls <pipgryllsastro@gmail.com>
2022-03-25 09:52:00 +0000

    script to refactor appWebComponets to objcts

--- src/app_modules/autoedit.mjs
+++ src/app_modules/autoedit.mjs
@@ -26,31 +26,16 @@ const apps = [caissonConf,
   vhmConf,
   ztiConf]
 
+
 for (let x in apps) {
-  let appConf = apps[x]
-  let modFields = ['radio-tile'];
-
+  let new_obj = {};
+  let appConf = apps[x];
+  let index=0;
   for (let component of appConf.appWebComponents) {
-    if (component.subComponents !== undefined) {
-      for (let subComponent of component.subComponents) {
-        let new_fields = {};
-        let modFieldIdx = modFields.indexOf(subComponent.type);
-        if (modFieldIdx >= 0) {
-          const groups = Object.entries(subComponent.options);
-          groups.forEach(([grp_key, value]) => {
-            new_fields[grp_key] = {
-              check_status: value[0],
-              label: value[1],
-              visible: value[2]
-            };
-          });
-          let mod_this = appConf.appWebComponents.find(element => element.type === component.type).subComponents.find(element => element.type === modFields[modFieldIdx]);
-          mod_this.options = new_fields;
-        }
-      }
-    }
+    new_obj[index] = component
+    index ++;
   }
-
+  appConf.appWebComponents = new_obj
   let newText = "/* eslint-disable no-sparse-arrays */\n" +
     "export const appConf = " + JSON.stringify(appConf, null, 2)
 
