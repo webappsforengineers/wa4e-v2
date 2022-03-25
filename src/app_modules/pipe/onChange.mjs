57c90961520e6b87ca85c56b61d45ea210844950
Pip Grylls <pipgryllsastro@gmail.com>
2022-03-25 20:51:01 +0000

    testing appconflint

--- src/app_modules/pipe/onChange.mjs
+++ src/app_modules/pipe/onChange.mjs
@@ -1,5 +1,5 @@
 // This object is used to get the fields that are modified on selection of a radio-tile
-// the structures are a subset of the appConf that are matched by modifyform to replace
+// the structures are a subset of the appConf that are merged by modifyform to replace
 // the correct element.
 
 const stabilityWeight = {
