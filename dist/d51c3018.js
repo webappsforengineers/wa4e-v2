12bb6d776f3623131c350201af308cc7982194fc
Pip Grylls <pipgryllsastro@gmail.com>
2022-05-23 12:28:49 +0100

    updating dist

---
+++ dist/d51c3018.js
@@ -0,0 +1,17 @@
+import { a as p } from './d1437ee2.js';
+import { C as e } from './c2a0d119.js';
+import { A as s } from './cd3c7a7a.js';
+class t extends s {
+  constructor() {
+    super(),
+      (this.appName = p.appName),
+      (this.title = p.appPageTitle),
+      (this.appWebComponents = p.appWebComponents),
+      (this.resetApp = p.appWebComponents),
+      (this.output = {}),
+      (this.appTiles = this.makeAppTiles()),
+      (this.appCalc = e);
+  }
+}
+customElements.define(`${p.appName}-app`, t);
+export { t as App };
