b4ef28f79dc3e0ee7728a5649f3c73976e6ea965
Pip Grylls <pipgryllsastro@gmail.com>
2022-05-24 10:29:51 +0100

    new rollup

---
+++ dist/0d526542.js
@@ -0,0 +1,17 @@
+import { a as p } from './deccfd8f.js';
+import { f as e } from './a27e039f.js';
+import { A as s } from './f8c198d2.js';
+class t extends s {
+  constructor() {
+    super(),
+      (this.appName = p.appName),
+      (this.title = p.appPageTitle),
+      (this.appWebComponents = p.appWebComponents),
+      (this.resetApp = p.appWebComponents),
+      (this.output = {}),
+      (this.appTiles = this.makeAppTiles()),
+      (this.appCalc = e);
+  }
+}
+customElements.define(`${p.appName}-app`, t);
+export { t as App };
