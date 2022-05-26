96617b7c311b47a6ceb8239d2a5ce878e2be78b4
Pip Grylls <pipgryllsastro@gmail.com>
2022-05-26 16:32:11 +0100

    new dist with no help

---
+++ dist/1b8b2f14.js
@@ -0,0 +1,18 @@
+import { a as p } from './65f25f2f.js';
+import { k as s, l as t } from './3ab7fe5c.js';
+import { A as e } from './a714b690.js';
+class a extends e {
+  constructor() {
+    super(),
+      (this.appName = p.appName),
+      (this.title = p.appPageTitle),
+      (this.appWebComponents = p.appWebComponents),
+      (this.resetApp = p.appWebComponents),
+      (this.output = {}),
+      (this.appTiles = this.makeAppTiles()),
+      (this.appCalc = s),
+      (this.appOptimize = t);
+  }
+}
+customElements.define(`${p.appName}-app`, a);
+export { a as App };
---
+++ dist/649673d6.js
@@ -0,0 +1,17 @@
+import { a as p } from './27875585.js';
+import { c as s } from './3ab7fe5c.js';
+import { A as e } from './a714b690.js';
+class t extends e {
+  constructor() {
+    super(),
+      (this.appName = p.appName),
+      (this.title = p.appPageTitle),
+      (this.appWebComponents = p.appWebComponents),
+      (this.resetApp = p.appWebComponents),
+      (this.output = {}),
+      (this.appTiles = this.makeAppTiles()),
+      (this.appCalc = s);
+  }
+}
+customElements.define(`${p.appName}-app`, t);
+export { t as App };
