92f1a3b068c3dbadbc69771cc092257cf4acd31a
PipGrylls <pipgryllsastro@gmail.com>
2022-06-08 11:00:19 +0000

    Deploying to gh-pages from @ webappsforengineers/wa4e-v2@2afcd195c56f845fdca30ed73741731d53a0d05f 🚀

---
+++ 8eaf6789.js
@@ -0,0 +1,18 @@
+import { a as p } from './27875585.js';
+import { A as s, l as t } from './0742f914.js';
+import { A as e } from './8e0b46b3.js';
+import './3f9f20b4.js';
+class a extends e {
+  constructor() {
+    super(),
+      (this.appName = p.appName),
+      (this.title = p.appPageTitle),
+      (this.appWebComponents = p.appWebComponents),
+      (this.resetApp = s(p.appWebComponents)),
+      (this.output = {}),
+      (this.appTiles = this.makeAppTiles()),
+      (this.appCalc = t);
+  }
+}
+customElements.define(`${p.appName}-app`, a);
+export { a as App };
