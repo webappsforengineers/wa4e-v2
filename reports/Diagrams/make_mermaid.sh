781beca8296d713f76d41b6b8d9f8d243c5501f9
PipGrylls <pipgryllsastro@gmail.com>
2022-12-19 16:34:56 +0000

    updated dependencies to get mermaid installed, requires testing

--- reports/Diagrams/make_mermaid.sh
+++ reports/Diagrams/make_mermaid.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
 # Documentation here https://github.com/mermaid-js/mermaid-cli#alternative-installations
 
-docker run --rm -u `id -u`:`id -g` -v C:\Users\pipgr\WebstormProjects\wa4e-v2\reports\Diagrams\mermaidFiles\:/data minlag/mermaid-cli -i C:\Users\pipgr\WebstormProjects\wa4e-v2\reports\Diagrams\mermaidFiles\structure.mmd
-
+mmdc -i ./mermaidFiles/structure.mmd -o ./structure.png
